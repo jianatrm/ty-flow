3b00b12ba3ecac28f5dd8dba1f4e8dec98b35014
yutingting <yutingting@360.cn>
2022-07-22 18:28:37 +0800

    feat:commit

--- src/components/X6Flow.vue
+++ src/components/X6Flow.vue
@@ -1,37 +1,134 @@
 <template>
  <div style="width: 100%;height: 100%;display: flex">
-   <div id="container"></div>
-   <div  v-if="formModel" class="detailPannel" style="width: 300px;height: 100%;background-color: #f2f3f5">
-    <div>{{formModel.type}}</div>
-
-     <el-form
-         label-position="top"
-         label-width="100px"
-         :model="formModel"
-         style="max-width: 460px"
-     >
-       <el-form-item label="标题">
-         <el-input :model-value="formModel.text" @input="handleInput"></el-input>
-       </el-form-item>
-       <el-form-item label="顺序">
-         <el-input v-model="formModel.region" />
-       </el-form-item>
-       <el-form-item label="Activity form">
-         <el-input v-model="formModel.type" />
-       </el-form-item>
-     </el-form>
+   <div id="container">
+     <div class="tool-bar">
+       <div class="bar-item">
+         <ZoomInOutlined :style="{fontSize: '16px'}"/>
+       </div>
+       <div class="bar-item">
+         <ZoomOutOutlined :style="{fontSize: '16px'}"/>
+       </div>
+       <div class="bar-item">
+         <SyncOutlined :style="{fontSize: '16px'}"/>
+       </div>
+     </div>
+   </div>
+   <div class="detail-panel" v-if="formModel">
+     <div class="detail-panel-title">{{translateFormTitle(formModel.type)}}</div>
+     <div class="detail-panel-content">
+       <a-form layout="vertical" :model="formModel" :rules="rules">
+         <a-form-item label="节点名称" name="text" v-if="formModel.type !== 'edge'">
+           <a-input v-model:value="formModel.text" placeholder="请输入" @blur="handleInput"/>
+         </a-form-item>
+         <a-form-item label="是否会签" name="sign" v-if="formModel.type === 'approve'">
+           <a-radio-group v-model:value="formModel.sign">
+             <a-radio :value="1">是</a-radio>
+             <a-radio :value="2">否</a-radio>
+           </a-radio-group>
+         </a-form-item>
+         <a-form-item label="选择审批人" name="approver" v-if="formModel.type === 'approve'">
+           <a-cascader
+               v-model:value="formModel.approver"
+               style="width: 100%"
+               multiple
+               max-tag-count="responsive"
+               :options="options"
+               placeholder="请选择"
+           ></a-cascader>
+         </a-form-item>
+         <a-form-item label="选择处理人" name="handler" v-if="formModel.type === 'task'">
+           <a-cascader
+               v-model:value="formModel.handler"
+               style="width: 100%"
+               multiple
+               max-tag-count="responsive"
+               :options="options"
+               placeholder="请选择"
+           ></a-cascader>
+         </a-form-item>
+         <a-form-item label="选择处理表单" name="formFields" v-if="formModel.type === 'task'">
+           <a-select
+               ref="select"
+               v-model:value="formModel.formFields"
+               placeholder="请选择"
+               style="width: 100%">
+             <a-select-option value="jack">处置阶段表单</a-select-option>
+             <a-select-option value="lucy">恢复阶段表单</a-select-option>
+             <a-select-option value="disabled">事后处理阶段表单</a-select-option>
+           </a-select>
+         </a-form-item>
+         <a-form-item label="属性" name="attr" v-if="formModel.type === 'edge'">
+           <a-select v-model:value="formModel.attr" placeholder="请选择" @change="handleAttrChange">
+             <a-select-option value="通过">通过</a-select-option>
+             <a-select-option value="不通过">不通过</a-select-option>
+             <a-select-option value="其他">其他</a-select-option>
+           </a-select>
+         </a-form-item>
+       </a-form>
+     </div>
    </div>
  </div>
+
+  <a-button @click="submit">提交</a-button>
 </template>
 
 <script setup>
+import {ZoomInOutlined, ZoomOutOutlined, SyncOutlined} from "@q/icons-vue"
 import {onMounted, reactive, ref, toRefs} from "vue";
 import {Graph, Shape, Addon} from '@antv/x6'
+import _ from "lodash";
+
 const selectModel = ref(null)
+
+const options = [
+  {
+    label: 'Light',
+    value: 'light',
+    children: new Array(20)
+        .fill(null)
+        .map((_, index) => ({ label: `Number ${index}`, value: index })),
+  },
+  {
+    label: 'Bamboo',
+    value: 'bamboo',
+    children: [
+      {
+        label: 'Little',
+        value: 'little',
+        children: [
+          {
+            label: 'Toy Fish',
+            value: 'fish',
+          },
+          {
+            label: 'Toy Cards',
+            value: 'cards',
+          },
+          {
+            label: 'Toy Bird',
+            value: 'bird',
+          },
+        ],
+      },
+    ],
+  },
+]
+
 const state = reactive({
-  formModel:null
+  formModel: null,
+  rules: {
+    text: [{required: true, message: '请输入节点名称', trigger: 'blur'}],
+    sign: [{required: true, message: '请选择是否会签'}],
+    approver: [{required: true, message: '请选择审批人'}],
+    handler: [{required: true, message: '请选择处理人'}],
+    formFields: [{required: true, message: '请选择处理表单'}],
+  },
+  edge: null,
+  graphInstance: null
 })
-const { formModel } =  toRefs(state)
+
+const { formModel, rules } =  toRefs(state)
+
 onMounted(() => {
   preWork()
 
@@ -157,7 +254,7 @@ onMounted(() => {
         height: 48,
         shape: 'rect',
         label: map[node.attrs.type].label,
-        data:{label: map[node.attrs.type].label,},
+        data: {label: map[node.attrs.type].label},
         attrs: {
           type:node.attrs.type,
           body: {
@@ -167,11 +264,11 @@ onMounted(() => {
             strokeWidth: 1,
             fill: '#ffffff',
             refWidth: 1,
-            refHeight: 1,
+            refHeight: 1
           },
           label:{
             refX: '100%',
-            refX2: -16,
+            refX2: -25,
             refY: 0.5,
             textAnchor: 'end',
             textVerticalAnchor: 'middle',
@@ -319,10 +416,18 @@ onMounted(() => {
       text:node.attrs.text.text,
       ...node.getData()
     }
+    console.log("all nodes is",  graph.getNodes())
+    console.log("all edges is",  graph.getEdges())
   })
   graph.on('edge:click', ({ edge, e }) => {
     console.log('edge:click',edge)
-    edge.setLabels('123')
+    state.edge = edge
+    state.formModel = {
+      type: edge.shape,
+      text: '连接线',
+      // ...edge.getData()
+    }
+    // edge.setLabels('123')
     // selectModel.value = node
     // state.formModel = {
     //   type:node.attrs.type,
@@ -413,11 +518,11 @@ onMounted(() => {
   const r1 = graph.createNode({
     shape: 'image',
     attrs: {
-      width:104,
-      type:'start',
+      width: 106,
+      type: 'start',
       label: {
         text: '开始节点',
-        fill: '#000000',
+        fill: '#525966',
         refX: 0.5,
         refY: 50,
         refY2: 4,
@@ -425,7 +530,7 @@ onMounted(() => {
         textVerticalAnchor: 'top',
       },
     },
-    width: 80,
+    width: 40,
     height: 40,
     imageUrl:new URL('../assets/x6/start.svg', import.meta.url).href,
   })
@@ -433,15 +538,15 @@ onMounted(() => {
     shape: 'image',
     x: 320,
     y: 120,
-    width: 80,
+    width: 40,
     height: 40,
     imageUrl:new URL('../assets/x6/approve.svg', import.meta.url).href,
     attrs: {
-      width:120,
+      width: 138,
       type:'approve',
       label: {
         text: '审批节点',
-        fill: '#000000',
+        fill: '#525966',
         refX: 0.5,
         refY: 50,
         refY2: 4,
@@ -454,15 +559,15 @@ onMounted(() => {
     shape: 'image',
     x: 320,
     y: 120,
-    width: 80,
+    width: 40,
     height: 40,
     imageUrl:new URL('../assets/x6/task.svg', import.meta.url).href,
     attrs: {
-      width:120,
+      width: 138,
       type:'task',
       label: {
         text: '处理节点',
-        fill: '#000000',
+        fill: '#525966',
         refX: 0.5,
         refY: 50,
         refY2: 4,
@@ -471,20 +576,19 @@ onMounted(() => {
       },
     },
   })
-
   const r4 = graph.createNode({
     shape: 'image',
     x: 320,
     y: 120,
-    width: 80,
+    width: 40,
     height: 40,
     imageUrl:new URL('../assets/x6/end.svg', import.meta.url).href,
     attrs: {
-      width:104,
+      width: 106,
       type:'end',
       label: {
         text: '结束节点',
-        fill: '#000000',
+        fill: '#525966',
         refX: 0.5,
         refY: 50,
         refY2: 4,
@@ -494,6 +598,13 @@ onMounted(() => {
     },
   })
   stencil.load([r1,r2,r3,r4], 'group1')
+
+  state.graphInstance = graph
+
+  console.log("localStorage",localStorage.getItem('dataSource'))
+  if(localStorage.getItem('dataSource')) {
+    graph.fromJSON(JSON.parse(localStorage.getItem('dataSource')))
+  }
 
 // #endregion
 
@@ -509,13 +620,36 @@ onMounted(() => {
   }
 })
 
+const translateFormTitle = (title) => {
+  if(title === 'start') return '开始节点'
+  if(title === 'approve') return '审批节点'
+  if(title === 'task') return '处理节点'
+  if(title === 'end') return '结束节点'
+  if(title === 'edge') return '连接线'
+}
 
+const handleInput = (e)=> {
+  console.log(e, "e is ")
+  selectModel.value.attr('text/text', e.target._value)
+  state.formModel.text = e.target._value
+  // _.debounce(() => {
+  //   selectModel.value.attr('text/text', e.target._value)
+  //   state.formModel.text = e.target._value
+  // }, 500)
+}
 
+const handleAttrChange = (val) => {
+  if(val !== '其他') state.edge.setLabels(val)
+}
 
-
-const handleInput = (e)=>{
-  selectModel.value.attr('text/text', e)
-  state.formModel.text = e
+const submit = () => {
+  console.log("当前所有节点", state.graphInstance.getNodes())
+  console.log("当前所有连接线", state.graphInstance.getEdges())
+  let data = state.graphInstance.toJSON(state.graphInstance.getNodes())
+  let data1 = state.graphInstance.toJSON(state.graphInstance.getEdges())
+  console.log(data)
+  console.log(data1)
+  localStorage.setItem('dataSource', JSON.stringify(data))
 }
 </script>
 
@@ -523,7 +657,9 @@ const handleInput = (e)=>{
 #container {
   display: flex;
   border: 1px solid #dfe3e8;
-  height: 100%
+  height: 100%;
+  position: relative;
+  z-index: 98;
 }
 
 #stencil {
@@ -541,16 +677,24 @@ const handleInput = (e)=>{
 
 .x6-widget-stencil {
   background-color: #f2f3f5 !important;
+  font-family: PingFang SC;
+  font-size: 14px;
+  color: #525966;
 }
 
 .x6-widget-stencil-title {
   background-color: #f2f3f5 !important;
-  height: 50px;
-  line-height: 50px;
+  height: 54px;
+  line-height: 54px;
   text-align: center;
+  font-family: PingFang SC;
+  font-size: 14px;
+  color: #242933;
+  font-weight: 500;
 }
-.x6-widget-stencil-content{
-  top:30px
+
+.x6-widget-stencil-content {
+  top: 40px
 }
 
 .x6-widget-stencil-group-title {
@@ -594,6 +738,57 @@ const handleInput = (e)=>{
 }
 .x6-widget-selection-selected{
   background-color:#cbe5dc ;
+}
 
+.tool-bar {
+  cursor: pointer;
+  position: absolute;
+  top: 17px;
+  right: 17px;
+
+  .bar-item {
+    display: inline-block;
+    background: #ffffff;
+    padding: 8px;
+    border: 1px solid #dfe1e6;
+    border-left: unset;
+    z-index: 99;
+    cursor: pointer;
+  }
+
+  .bar-item:first-child {
+    border-left: 1px solid #dfe1e6;
+    border-top-left-radius: 2px;
+    border-bottom-left-radius: 2px;
+  }
+
+  .bar-item:last-child {
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
+  }
+
+  .bar-active {
+    border: 1px solid #00AB7A!important;
+    color: #00AB7A;
+  }
+}
+
+.detail-panel {
+  width: 300px;
+  height: 100%;
+  background-color: #f2f3f5;
+
+  .detail-panel-title {
+    padding: 10px 24px;
+    border-bottom: 1px solid #dfe1e6;
+    font-family: PingFang SC;
+    font-size: 14px;
+    font-weight: 500;
+    color: #242933;
+  }
+
+  .detail-panel-content {
+    padding: 20px 24px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+
+import Design from '@q/design'
+import '@q/design/dist/design.css'
+
 const app = createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
+app.use(Design).mount('#app')
